d375bf136eb4fb80e69c9560ae4dabc8fbad8002
frederikhendrix <rikhendrix@live.nl>
2024-05-12 22:29:20 +0200

    added possible null value of Text

--- ReviewService/ReviewService/Models/Review.cs
+++ ReviewService/ReviewService/Models/Review.cs
@@ -5,7 +5,7 @@
         public Guid ReviewId { get; set; }
         public Guid UserId { get; set; }
         public Guid MovieId { get; set; }
-        public string Text { get; set; }
+        public string? Text { get; set; }
         public int Score { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
