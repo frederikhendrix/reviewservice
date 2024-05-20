5fdb1d61088b582f0e2620e8911e5209facedf0c
frederikhendrix <rikhendrix@live.nl>
2024-05-20 22:07:02 +0200

    Test changed so it passes with new MessageSender

--- ReviewServiceTest/ReviewControllerTest.cs
+++ ReviewServiceTest/ReviewControllerTest.cs
@@ -1,43 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using ReviewService.Bus;
 using ReviewService.Controllers;
-using ReviewService.Data;
 using ReviewService.Interfaces;
 using ReviewService.Models;
-using ReviewService.Services;
+using System;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace ReviewServiceTest
 {
     public class ReviewControllerTest
     {
-
         private readonly Mock<IReviewService> _mockReviewService;
+        private readonly Mock<MessageSender> _mockMessageSender;
         private readonly ReviewsController _controller;
-
 
         public ReviewControllerTest()
         {
             _mockReviewService = new Mock<IReviewService>();
-            _controller = new ReviewsController(_mockReviewService.Object);
+
+            // Provide mock values for the constructor parameters
+            var mockServiceBusConnectionString = "Endpoint=sb://mock.servicebus.windows.net/;SharedAccessKeyName=MockKey;SharedAccessKey=MockAccessKey";
+            var mockTopicName = "mock-topic";
+
+            _mockMessageSender = new Mock<MessageSender>(mockServiceBusConnectionString, mockTopicName);
+            _controller = new ReviewsController(_mockReviewService.Object, _mockMessageSender.Object);
         }
 
-        /// <summary>
-        /// This function calls GetReviewByIdAsync with .ReturnsAsync((Review)null) so the function always returns nothing.
-        /// This is to make sure the Assert.IsType always passes.
-        /// </summary>
-        /// <returns>True</returns>
         [Fact]
         public async Task GetReviewByIdAsync_Test_Function_That_Returns_No_Review()
         {
-            //Arrange 
+            // Arrange 
             _mockReviewService.Setup(x => x.GetReviewByIdAsync(It.IsAny<Guid>())).ReturnsAsync((Review)null);
 
-            //Act
+            // Act
             var result = await _controller.GetReview(Guid.NewGuid());
 
-            //Assert
+            // Assert
             Assert.IsType<NotFoundResult>(result);
-
         }
     }
 }
